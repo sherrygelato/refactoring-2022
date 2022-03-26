34a903d3dc1ee030869dd69a039d397fb6df36b3
sherrygelato <sherrygelato@kakao.com>
2022-03-26 10:15:38 +0900

    [2일차] 15. code.refactoring.classmove2

--- src/code/refactoring/classmove_2/AccountType.java
+++ src/code/refactoring/classmove_2/AccountType.java
@@ -2,4 +2,14 @@ package code.refactoring.classmove_2;
 
 public class AccountType {
 
+    // Account에서 이동, 이동 후 [소스작업]의 getter setter 잊지 말자
+    private double _interestRate = 4.53;
+
+    public double get_interestRate() {
+        return _interestRate;
+    }
+
+    public void set_interestRate(double _interestRate) {
+        this._interestRate = _interestRate;
+    }
 }
