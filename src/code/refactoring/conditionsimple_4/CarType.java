2663571a435a3f3ba61ed846389a914b7e527f82
sherrygelato <sherrygelato@kakao.com>
2022-03-26 14:21:15 +0900

    [2일차] 23. code.refactoring.conditionsimple4 다른 버전으로 리팩토링

--- src/code/refactoring/conditionsimple_4/CarType.java
+++ src/code/refactoring/conditionsimple_4/CarType.java
@@ -2,8 +2,24 @@ package code.refactoring.conditionsimple_4;
 
 public abstract class CarType {
 
+    public static final int COMPACTCAR = 1;
+	public static final int SEDANCAR = 2;
+	public static final int SPORTCAR = 3;
     int currentSpeed = 0;
 
     public abstract int speedUp();
+
+    public static CarType setType(int key) {
+        switch(key) {
+        case COMPACTCAR :
+            return new CompactCar();
+        case SEDANCAR :
+            return new SedanCar();
+        case SPORTCAR :
+            return new SportCar();
+        default:
+            throw new IllegalArgumentException("Incorrec Car Type");
+        }
+    }
     
 }
--- test/code/refactoring/conditionsimple_4/CarClient.java
+++ test/code/refactoring/conditionsimple_4/CarClient.java
@@ -3,28 +3,30 @@ package code.refactoring.conditionsimple_4;
 import java.util.Scanner;
 
 public class CarClient {
-	public static final int COMPACTCAR = 1;
-	public static final int SEDANCAR = 2;
-	public static final int SPORTCAR = 3;
+	// public static final int COMPACTCAR = 1;
+	// public static final int SEDANCAR = 2;
+	// public static final int SPORTCAR = 3;
 
 	public static void main(String[] args) {
 		
 		CarType cartype = null;
 		Scanner input = new Scanner(System.in);
 		int key = input.nextInt();
+
+		cartype = CarType.setType(key);
 		  
 		// 경우의 수 조건 따라 처리 되는 파일 클래스 결정
-		switch(key) {
-		case COMPACTCAR :
-			cartype = new CompactCar();
-			break;
-		case SEDANCAR :
-			cartype = new SedanCar();
-			break;
-		case SPORTCAR :
-			cartype = new SportCar();
-			break;
-		}
+		// switch(key) {
+		// case COMPACTCAR :
+		// 	cartype = new CompactCar();
+		// 	break;
+		// case SEDANCAR :
+		// 	cartype = new SedanCar();
+		// 	break;
+		// case SPORTCAR :
+		// 	cartype = new SportCar();
+		// 	break;
+		// }
 		
 		System.out.println("선택한 자동차의 속도는 "+ cartype.speedUp() + " 입니다.");
 	}
