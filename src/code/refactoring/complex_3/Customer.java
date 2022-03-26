95bc1377c18cfd569fc33f61745c74d44b087051
sherrygelato <sherrygelato@kakao.com>
2022-03-26 17:00:24 +0900

    [2일차] 32. code.refactoring.complex3 - (3)

--- src/code/refactoring/complex_3/Customer.java
+++ src/code/refactoring/complex_3/Customer.java
@@ -20,30 +20,48 @@ public class Customer {
 		return _name;
 	}
 	
-	public String statement() {
+	public double getTotalRentalCharge() {
 		double totalAmount = 0;
-		int totalRentalPoint = 0;
 		Enumeration rentals = _rentals.elements();
-		String result = getName() + " 고객님의 대여기록\n";
-		
+
 		while (rentals.hasMoreElements()) {
-			// double thisAmount = 0;
-			// int thisRentalPoint = 0;
 			Rental aRental = (Rental)rentals.nextElement();
 			
 			// 비디오 종류별 대여료 계산
 			double thisAmount = aRental.getRentalCharge();
+			totalAmount += thisAmount;
+		}
+		return totalAmount;
+	}
+
+	public int getTotalRentalPoint() {
+		int totalRentalPoint = 0;
+		Enumeration rentals = _rentals.elements();
+		
+		while (rentals.hasMoreElements()) {
+			Rental aRental = (Rental)rentals.nextElement();
+			
 			// 적립 포인트를 1 포인트 증가
 			int thisRentalPoint = aRental.getRentalPoint();
 			
-			//이번에 대여하는 비디오정보와 대여료를 출력
-			result += "\t" + aRental.getMovie().getTitle()+ "\t" + String.valueOf(thisAmount) + "\n";
-			//현재까지 누적된 총 대여료
-			totalAmount += thisAmount;
 			totalRentalPoint += thisRentalPoint;
-			//푸터행 추가
-			result += "누적 대여료: " + String.valueOf(totalAmount) + "\n";
-			result += "적립 포인트: " + String.valueOf(thisRentalPoint) + "\n";
+		}
+		return totalRentalPoint;
+	}
+
+	// 출력
+	public String statement() {
+		Enumeration rentals = _rentals.elements();
+		String result = getName() + " 고객님의 대여기록\n";
+		
+		while (rentals.hasMoreElements()) {
+			Rental aRental = (Rental)rentals.nextElement();
+
+			//이번에 대여하는 비디오정보와 대여료를 출력
+			result += "\t" + aRental.getMovie().getTitle()+ "\t" + String.valueOf(aRental.getRentalCharge()) + "\n";
+			// //푸터행 추가
+			// result += "누적 대여료: " + String.valueOf(getTotalRentalCharge()) + "\n";
+			// result += "적립 포인트: " + String.valueOf(getTotalRentalPoint()) + "\n";
 		}
 		return result;
 	}
