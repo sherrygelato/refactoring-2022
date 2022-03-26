704bfd1463c6e13eaa946e5d847ed492834f0bd3
sherrygelato <sherrygelato@kakao.com>
2022-03-26 17:19:48 +0900

    [2일차] 32. code.refactoring.complex3 - (4)

--- src/code/refactoring/complex_3/Movie.java
+++ src/code/refactoring/complex_3/Movie.java
@@ -23,4 +23,34 @@ public class Movie {
 	public String getTitle() {
 		return _title;
 	}
+
+	double getRentalCharge(int _daysRented) {
+		double thisAmount = 0;
+
+		switch(_priceCode) {
+		case REQULAR:
+			thisAmount += 2;
+			if(_daysRented > 2)
+				thisAmount += (_daysRented-2) * 1.5;
+			break;
+		case NEW_RELEASE:
+			thisAmount += (_daysRented) * 3;
+			break;
+		case CHILDERNS:
+			thisAmount += 1.5;
+			if(_daysRented > 3)
+				thisAmount += (_daysRented-3) * 1.5;
+			break;
+		}
+		return thisAmount;
+	}
+
+	int getRentalPoint(int _daysRented) {
+		int thisRentalPoint = 0;
+		thisRentalPoint ++;
+		//최신물을 이틀이상 대여하면 보너스포인트 지급
+		if((_priceCode == Movie.NEW_RELEASE) && _daysRented > 1)
+			thisRentalPoint ++;
+		return thisRentalPoint;
+	}
 }
--- src/code/refactoring/complex_3/Rental.java
+++ src/code/refactoring/complex_3/Rental.java
@@ -3,7 +3,6 @@ package code.refactoring.complex_3;
 public class Rental {
 	private Movie _movie;
 	private int _daysRented;
-	private Rental aRental;
 	
 	public Rental(Movie movie, int daysRented) {
 		_movie = movie;
@@ -19,32 +18,11 @@ public class Rental {
 	}
 
 	double getRentalCharge() {
-		double thisAmount = 0;
+		return _movie.getRentalCharge(_daysRented);
 
-		switch(aRental.getMovie().getPriceCode()) {
-		case Movie.REQULAR:
-			thisAmount += 2;
-			if(aRental.getDaysRented() > 2)
-				thisAmount += (aRental.getDaysRented()-2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += (aRental.getDaysRented()) * 3;
-			break;
-		case Movie.CHILDERNS:
-			thisAmount += 1.5;
-			if(aRental.getDaysRented() > 3)
-				thisAmount += (aRental.getDaysRented()-3) * 1.5;
-			break;
-		}
-		return thisAmount;
 	}
 
 	int getRentalPoint() {
-		int thisRentalPoint = 0;
-		thisRentalPoint ++;
-		//최신물을 이틀이상 대여하면 보너스포인트 지급
-		if((aRental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && aRental.getDaysRented() > 1)
-		thisRentalPoint ++;
-		return thisRentalPoint;
+		return _movie.getRentalPoint(_daysRented);
 	}
 }
